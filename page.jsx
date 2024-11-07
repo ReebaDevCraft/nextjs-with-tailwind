5aa5075524118d3864fb995b62c604ce5d3da86a
Reeba <167805857+ReebaDevCraft@users.noreply.github.com>
2024-11-07 16:07:44 +0500

    Add files via upload

---
+++ page.jsx
@@ -0,0 +1,10 @@
+const AboutCollege=()=>{
+    return(
+        <div>
+            <h1>
+                This page is about nested routing
+            </h1>
+        </div>
+    )
+}
+export default AboutCollege;
